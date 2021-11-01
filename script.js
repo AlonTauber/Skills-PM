7de6fbd8807a24e884f4eb98fb4ab01c88a36bb8
AlonTauber <alonta@moveo.co.il>
2021-11-01 10:39:44 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function changecolor() {
+    const resetClass = 'reset-color';
+    let colors = document.querySelectorAll(".orange, .red, .green");
+    // let orange = document.getElementsByClassName("orange");
+    // let red = document.getElementsByClassName("red");
+    // let green = document.getElementsByClassName("green");
+    let btntext = document.getElementById("button");
+
+    btntext.classList.toggle("resetbutton");
+
+    if(btntext.value === "reset color"){
+        btntext.value = "return color";
+    } else {
+        btntext.value = "reset color"
+    }
+  
+    for (let i = 0; i < colors.length; i++) {
+        const element = colors[i];
+        element.classList.toggle(resetClass)
+    }
+
+    // for (let i = 0; i < orange.length; i++) {
+    //     const element = orange[i];
+    //     element.classList.toggle(resetClass)
+    // }
+
+    // for (let i = 0; i < red.length; i++) {
+
+    //     const element = red[i];
+    //     element.classList.toggle(resetClass)
+    // }
+    // for (let i = 0; i < green.length; i++) {
+
+    //     const element = green[i];
+    //     element.classList.toggle(resetClass)
+    // }
+    
+    
+}
