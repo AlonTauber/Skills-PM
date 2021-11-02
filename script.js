5a0c11a9a15f81108f03c6ffbcfa063391f5298c
AlonTauber <alonta@moveo.co.il>
2021-11-02 15:20:07 +0200

    CHANGES

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function changecolor() {
     const resetClass = 'reset-color';
-    let colors = document.querySelectorAll(".orange, .red, .green");
-    // let orange = document.getElementsByClassName("orange");
-    // let red = document.getElementsByClassName("red");
-    // let green = document.getElementsByClassName("green");
+    // let colors = document.querySelectorAll(".orange, .red, .green");
+    let orange = document.getElementsByClassName("orange");
+    let red = document.getElementsByClassName("red");
+    let green = document.getElementsByClassName("green");
     let btntext = document.getElementById("button");
 
     btntext.classList.toggle("resetbutton");
@@ -14,26 +14,28 @@ function changecolor() {
         btntext.value = "reset color"
     }
   
-    for (let i = 0; i < colors.length; i++) {
-        const element = colors[i];
+    // for (let i = 0; i < colors.length; i++) {
+    //     const element = colors[i];
+    //     element.classList.toggle(resetClass)
+    // }
+
+    for (let i = 0; i < orange.length; i++) {
+        const element = orange[i];
         element.classList.toggle(resetClass)
     }
 
-    // for (let i = 0; i < orange.length; i++) {
-    //     const element = orange[i];
-    //     element.classList.toggle(resetClass)
-    // }
+    for (let i = 0; i < red.length; i++) {
 
-    // for (let i = 0; i < red.length; i++) {
+        const element = red[i];
+        element.classList.toggle(resetClass)
+    }
+    for (let i = 0; i < green.length; i++) {
 
-    //     const element = red[i];
-    //     element.classList.toggle(resetClass)
-    // }
-    // for (let i = 0; i < green.length; i++) {
+        const element = green[i];
+        element.classList.toggle(resetClass)
+    }
 
-    //     const element = green[i];
-    //     element.classList.toggle(resetClass)
-    // }
+    //changed it back to seperates loops//
     
     
 }
